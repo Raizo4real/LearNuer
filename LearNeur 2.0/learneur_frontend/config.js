1a7b730acb02a7b7c86b8c356d7bb85489591912
Raizo <raizo4real@gmail.com>
2026-06-19 20:50:46 +0300

    config.js

--- LearNeur 2.0/learneur_frontend/config.js
+++ LearNeur 2.0/learneur_frontend/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // لينك الباك إند بتاعك (FastAPI)
-    API_BASE_URL: 'http://127.0.0.1:8000',
+    API_BASE_URL: 'learnuer-production.up.railway.app',
     
     // لينك الفرونت إند بتاعك (Live Server)
     FRONTEND_URL: 'http://127.0.0.1:5500' 
